48029297bfdf3d822f0ecf111d40209b62bef9eb
Brenan Sano <brenan_sano@example.com>
2026-08-07 15:17:14 -0700

    Revert history icon to 🕘

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Route Optimizer PWA
-const CACHE_NAME = 'route-optimizer-v76';
+const CACHE_NAME = 'route-optimizer-v77';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
