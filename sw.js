bb209bbdc7e231877d1793cbb11f923e6b550ab7
Brenan Sano <brenan_sano@example.com>
2026-08-07 13:13:49 -0700

    Bump SW cache v48 to ensure overwrite confirm is deployed

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Route Optimizer PWA
-const CACHE_NAME = 'route-optimizer-v47';
+const CACHE_NAME = 'route-optimizer-v48';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
