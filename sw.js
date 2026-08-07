76d7cb0ecf87004e1f24b51735c8d0312cd03ea1
Brenan Sano <brenan_sano@example.com>
2026-08-07 15:13:33 -0700

    Reduce space between optimize button and map to match above spacing

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Route Optimizer PWA
-const CACHE_NAME = 'route-optimizer-v74';
+const CACHE_NAME = 'route-optimizer-v75';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
