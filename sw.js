c1099fb4116f98ea0c753a59de836baec8d87b12
Brenan Sano <brenan_sano@example.com>
2026-08-07 13:38:28 -0700

    Make clear X larger (1.6rem) keeping same hitbox

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Route Optimizer PWA
-const CACHE_NAME = 'route-optimizer-v58';
+const CACHE_NAME = 'route-optimizer-v59';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
