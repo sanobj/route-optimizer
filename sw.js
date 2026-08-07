32daf56a7e5e0f5676e903966b7d13d377fc032a
Brenan Sano <brenan_sano@example.com>
2026-08-07 15:15:42 -0700

    Change history icon back to clock (🕐)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Route Optimizer PWA
-const CACHE_NAME = 'route-optimizer-v75';
+const CACHE_NAME = 'route-optimizer-v76';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
