980303e09cc8b3a0afa1683f44d6677b1db10db1
Brenan Sano <brenan_sano@example.com>
2026-08-07 15:57:39 -0700

    Restructure Advanced UI info as nested bullet list

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Route Optimizer PWA
-const CACHE_NAME = 'route-optimizer-v83';
+const CACHE_NAME = 'route-optimizer-v85';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
