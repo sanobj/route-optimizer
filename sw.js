9ad63b7b0c3498e4aa02290e51f42f8496abf02c
Brenan Sano <brenan_sano@example.com>
2026-08-07 11:38:38 -0700

    Bump SW cache to v19

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Route Optimizer PWA
-const CACHE_NAME = 'route-optimizer-v18';
+const CACHE_NAME = 'route-optimizer-v19';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
