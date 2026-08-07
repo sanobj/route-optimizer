64dd68666f97e737e074ba011ffddd41657de87a
Brenan Sano <brenan_sano@example.com>
2026-08-07 13:07:00 -0700

    Change saved routes header icon to floppy disk (💾)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Route Optimizer PWA
-const CACHE_NAME = 'route-optimizer-v43';
+const CACHE_NAME = 'route-optimizer-v44';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
