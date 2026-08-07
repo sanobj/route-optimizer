10403257fcc581eae5a24b1bfaf2a289a0d44270
Brenan Sano <brenan_sano@example.com>
2026-08-07 13:33:34 -0700

    Move Clear button next to Add Stop, restore full-width Optimize

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Route Optimizer PWA
-const CACHE_NAME = 'route-optimizer-v55';
+const CACHE_NAME = 'route-optimizer-v56';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
