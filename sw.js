854ee1e32b74cf7216c69f7c3c4f27d5ac0600b8
Brenan Sano <brenan_sano@example.com>
2026-08-07 12:07:26 -0700

    Use headerDisabled on InfoWindow to remove whitespace, tap map to close

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Route Optimizer PWA
-const CACHE_NAME = 'route-optimizer-v27';
+const CACHE_NAME = 'route-optimizer-v28';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
