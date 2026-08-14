49f0a5fa5088389a52b03c072dfc6f1e6359d77f
Brenan Sano <brenan_sano@example.com>
2026-08-14 09:20:40 -0700

    Don't count start location as a stop in summary

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Route Optimizer PWA
-const CACHE_NAME = 'route-optimizer-v99';
+const CACHE_NAME = 'route-optimizer-v100';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
